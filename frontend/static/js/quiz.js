db825587296f9c0725a37f819b60052e98208ed0
Sophia Caspar <sophiacaspar@hotmail.com>
2017-11-28 21:10:17 +0100

    setup for adding more questions

--- frontend/static/js/quiz.js
+++ frontend/static/js/quiz.js
@@ -10,14 +10,34 @@ function getAnswers(ans){
 		case 0:
 			obj.q1 = ans;
 			var current = document.getElementById('test1');
+			current.id = 'test2';
 			var next = document.getElementById('test2');
-			current.style.display = "none";
-			next.style.display = "block";
+
+			document.getElementById('ans1').innerHTML = "test";
+			document.getElementById('ans1').setAttribute('onclick', "showAnswers('test')");
+			//current.style.display = "none";
+			//next.style.display = "block";
 			q++;
 			break;
 		case 1:
 			break;
 	}
+}
+
+function setNewQuestion(question){
+
+}
+
+function setNewID(current, next){
+
+}
+
+function setNewAnswer(id, answer){
+
+}
+
+function setNewOnclickAttribute(id, shortAnswer, lastQuestion){
+
 }
 
 function showAnswers(ans) {
